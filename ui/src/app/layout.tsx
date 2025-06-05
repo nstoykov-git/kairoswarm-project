71e0682370215f78d4d056cae9caf08ff8d0c1d0
Nikolay Stoykov Stoykov <nstoykov@outlook.com>
2025-06-04 20:06:39 -0500

    Persistent routes

--- ui/src/app/layout.tsx
+++ ui/src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-       {/* <ToasterWrapper />*/}
+        <ToasterWrapper />
         {children}
       </body>
     </html>
