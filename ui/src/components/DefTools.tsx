eae3e95d2d2893a7e7333218af80e11c2910b358
Nikolay Stoykov Stoykov <nstoykov@outlook.com>
2025-06-24 13:34:53 -0500

    Subscription

--- ui/src/components/DefTools.tsx
+++ ui/src/components/DefTools.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 // src/components/DefTools.tsx
+import { supabase } from "@/lib/supabase";
 import { useEffect } from "react";
 import toast from "react-hot-toast";
 import { useState } from "react";
@@ -11,7 +12,6 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import { Radar, RadarChart, PolarGrid, PolarAngleAxis, PolarRadiusAxis, ResponsiveContainer } from 'recharts';
-import { supabase } from "@/lib/supabase";
 
 const traits = [
   { key: "openness", label: "Openness" },
