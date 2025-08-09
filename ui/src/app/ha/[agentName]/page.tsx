896fadc4f3ded6630b984dc1de58b67e464320b3
Nikolay Stoykov Stoykov <nstoykov@outlook.com>
2025-08-09 18:42:45 -0500

    HA Link

--- ui/src/app/ha/[agentName]/page.tsx
+++ ui/src/app/ha/[agentName]/page.tsx
@@ -1,11 +1,5 @@
 import SingleAgentIntro from "@/components/SingleAgentIntro";
 
-type PageProps = {
-  params: {
-    agentName: string;
-  };
-};
-
-export default function AgentPage({ params }: PageProps) {
+export default function AgentPage({ params }: { params: { agentName: string } }) {
   return <SingleAgentIntro agentName={params.agentName} />;
 }
